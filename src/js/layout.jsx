52c11677f85245964e05dc80b017249c658c4e99
Kacey Dawson <dawson.kacey@gmail.com>
2019-03-07 00:58:00 +0000

    further changes

--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ScrollToTop from "./component/functional/scrollToTop.jsx";
 
-import { Home } from "./views/home.jsx";
-import { Demo } from "./views/demo.jsx";
-import { Single } from "./views/single.jsx";
+import { Home } from "./views/landing.jsx";
+import { Cart } from "./views/cart.jsx";
+//import { Single } from "./views/single.jsx";
 import Store from "./store/appContext.jsx";
 
 import { Navbar } from "./component/navbar.jsx";
@@ -19,8 +19,10 @@ export class Layout extends React.Component {
 					<Navbar />
 					<Switch>
 						<Route exact path="/" component={Home} />
-						<Route path="/demo" component={Demo} />
-						<Route path="/single/:theid" component={Single} />
+						<Route exact path="/cart" component={Cart} />
+						<Route exact path="/" component={Home} />
+						<Route exact path="/" component={Home} />
+						
 						<Route render={() => <h1>Not found!</h1>} />
 					</Switch>
 					<Footer />
