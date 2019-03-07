e2448b9e40adc740ef5072e7c598ec6590616871
Angelo Martinez <amart670@fiu.edu>
2019-03-07 19:48:31 +0000

    fixed navbar, added blog section

--- src/js/views/blog.jsx
+++ src/js/views/blog.jsx
@@ -3,6 +3,36 @@ import { Link } from "react-router-dom";
 
 export class Blog extends React.Component {
 	render() {
-		return <h1>test</h1>;
+		return (
+			<div>
+				<div className="jumbotron jumbotron-fluid">
+					<div className="container">
+						<h1 className="display-4">#LearnToCode</h1>
+						<p className="lead">
+							This is a modified jumbotron that occupies the
+							entire horizontal space of its parent.
+						</p>
+					</div>
+				</div>
+				<div className="card text-center">
+					<img
+						className="card-img-top"
+						src="https://via.placeholder.com/350x150"
+						alt="Card image cap"
+					/>
+					<div className="card-body">
+						<h5 className="card-title">Blog Post name</h5>
+						<p className="card-text">
+							With supporting text below as a natural lead-in to
+							additional content.lorem ipsum,.
+						</p>
+						<button className="btn btn-primary">
+							check out the full story
+						</button>
+					</div>
+					<div className="card-footer text-muted">6 days ago</div>
+				</div>
+			</div>
+		);
 	}
 }
