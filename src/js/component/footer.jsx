bc92a7d3ccbdcd6eaa5848436c5d852b5d336e41
Andy Martinez <martinez1751@gmail.com>
2019-03-09 00:55:40 +0000

    added footer

--- src/js/component/footer.jsx
+++ src/js/component/footer.jsx
@@ -1,8 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
+//style sheet
+
+import "../../styles/footer.scss";
 
 export class Footer extends React.Component {
 	render() {
-		return <h2 className="text-center">Footer</h2>;
+		return (
+			<section id="footer">
+				<div className="container">
+					<div className="row text-center text-xs-center text-sm-left text-md-left">
+						<div className="col-xs-12 col-sm-4 col-md-4">
+							<ul className="list-unstyled list-inline quick-links">
+								<li className="list-inline-item">
+									<a href="javascript:void();">Home</a>
+								</li>
+								<li className="list-inline-item">
+									<a href="javascript:void();">About</a>
+								</li>
+								<li className="list-inline-item">
+									<a href="javascript:void();">Contact Us</a>
+								</li>
+								<li className="list-inline-item">
+									<a href="javascript:void();">Blog</a>
+								</li>
+								<li className="list-inline-item">
+									<a href="javascript:void();">Cart</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+		);
 	}
 }
