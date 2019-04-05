1f28a22f04e5c630757a2a04f5ec877dbe397c1b
ENRIQUE LINARES <linarescarmona@gmail.com>
2019-04-05 18:24:10 +0000

    product page not loading

--- src/js/views/product.jsx
+++ src/js/views/product.jsx
@@ -1,105 +1,37 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Carousel from 'react-bootstrap/Carousel';
 
 //import Navbar from "../component/navbar.jsx";
 //import Footer from "./src/js/component/functional/footer.jsx";
 import "../../styles/product.scss";
 
-export class Product extends React.Component {
+export class Products extends React.Component {
 	render() {
 		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-sm-3">
-						<h1>here goes the product image</h1>
-					</div>
-					<div className="col-sm-9" />
-					<div className="content-wrapper">
-						<div className="item-container">
-							<div className="container">
-								<div className="col-md-12">
-									<div className="product col-md-3 service-image-left">
-										<center>
-											<img
-												id="item-display"
-												src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png"
-												alt=""
-											/>
-										</center>
-									</div>
-
-									<div className="container service1-items col-sm-2 col-md-2 pull-left">
-										<center>
-											<a
-												id="item-1"
-												className="service1-item">
-												<img
-													src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png"
-													alt=""
-												/>
-											</a>
-											<a
-												id="item-2"
-												className="service1-item">
-												<img
-													src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png"
-													alt=""
-												/>
-											</a>
-											<a
-												id="item-3"
-												className="service1-item">
-												<img
-													src="http://www.corsair.com/Media/catalog/product/g/s/gs600_psu_sideview_blue_2.png"
-													alt=""
-												/>
-											</a>
-										</center>
-									</div>
-								</div>
-
-								<div className="col-md-7">
-									<div className="product-title">
-										Corsair GS600 600 Watt PSU
-									</div>
-									<div className="product-desc">
-										The Corsair Gaming Series GS600 is the
-										ideal price/performance choice for
-										mid-spec gaming PC
-									</div>
-									<div className="product-rating">
-										<i className="fa fa-star gold" />{" "}
-										<i className="fa fa-star gold" />{" "}
-										<i className="fa fa-star gold" />{" "}
-										<i className="fa fa-star gold" />{" "}
-										<i className="fa fa-star-o" />{" "}
-									</div>
-									<hr />
-									<div className="product-price">
-										$ 1234.00
-									</div>
-									<div className="product-stock">
-										In Stock
-									</div>
-									<hr />
-									<div className="btn-group cart">
-										<button
-											type="button"
-											className="btn btn-success">
-											Add to cart
-										</button>
-									</div>
-									<div className="btn-group wishlist">
-										<button
-											type="button"
-											className="btn btn-danger">
-											Add to wishlist
-										</button>
-									</div>
-								</div>
-							</div>
+			<div className="container-fluid">
+					<div className= "row">
+					<div className= "col-md-5">
+					   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+							  <div className="carousel-inner">
+							    <div className="carousel-item active">
+							      <img src="https://via.placeholder.com/150" className="d-block w-100" alt="..."/>
+							    </div>
+							  </div>
+							  <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+							    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+							    <span className="sr-only">Previous</span>
+							  </a>
+							  <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+							    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+							    <span className="sr-only">Next</span>
+							  </a>
 						</div>
 					</div>
+					<div className= "col-md-5">
+							test
+				
+					</div>				
 				</div>
 			</div>
 		);
