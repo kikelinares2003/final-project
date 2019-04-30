ccd5d53179fa32b7536897bdb95a939ad507de37
Kacey Dawson <dawson.kacey@gmail.com>
2019-04-30 14:39:18 +0000

    initial check-out page commit

---
+++ src/js/component/co-cart.jsx
@@ -0,0 +1,72 @@
+//create your first component
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import { Context } from "../store/appContext.jsx";
+
+//include images into your bundle
+//import capeImg from "../../img/cape_inView.jpg";
+
+export class CheckOutCart extends React.Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div className="col-md-4 order-md-2 mb-4">
+					<h4 className="d-flex justify-content-between align-items-center mb-3">
+						<span className="text-muted">Your cart</span>
+						<span className="badge badge-secondary badge-pill">
+							3
+						</span>
+					</h4>
+
+					<ul className="list-group mb-3">
+						<Context.Consumer>
+							<React.Fragment>
+								{({ store, actions }) => {
+									return store.cartStore.map(
+										(item, index) => {
+											return (
+												<li
+													className="list-group-item d-flex justify-content-between lh-condensed"
+													key={index}>
+													<div>
+														<h6 className="my-0">
+															{item.post_title}
+														</h6>
+													</div>
+													<span className="text-muted">
+														{item.post_title}
+													</span>
+												</li>
+											);
+										}
+									);
+								}}
+								<li className="list-group-item d-flex justify-content-between">
+									<span>Total (USD)</span>
+									<strong>$20</strong>
+								</li>
+							</React.Fragment>
+						</Context.Consumer>
+					</ul>
+
+					<form className="card p-2">
+						<div className="input-group">
+							<input
+								type="text"
+								className="form-control"
+								placeholder="Promo code"
+							/>
+							<div className="input-group-append">
+								<button
+									type="submit"
+									className="btn btn-secondary">
+									Redeem
+								</button>
+							</div>
+						</div>
+					</form>
+				</div>
+			</React.Fragment>
+		);
+	}
+}
